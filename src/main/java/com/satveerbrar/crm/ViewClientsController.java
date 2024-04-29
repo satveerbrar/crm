17fef78c6ee1f1b24cc82139f349b3c712888c92
Satveer Brar <satveerbrar180@gmail.com>
2024-04-29 13:04:57 -0400

    File name to ViewClientsController

--- src/main/java/com/satveerbrar/crm/ViewClientsController.java
+++ src/main/java/com/satveerbrar/crm/ViewClientsController.java
@@ -4,7 +4,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -15,7 +14,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
-public class ViewCustomersController implements Initializable {
+public class ViewClientsController implements Initializable {
     @FXML
     private TableView<Client> clientsTable;
     @FXML
