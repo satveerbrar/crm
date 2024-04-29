8f0f8bb79a0ec95faafa32dac8ef8245c3dfa006
Satveer Brar <satveerbrar180@gmail.com>
2024-04-29 10:15:21 -0400

    View Clients feature implemented - Client class created with constructor and getter/setters - FXML file created with tableView to map all the clients infomation - Controller created to pull all clients info from SQL and bind with table

---
+++ src/main/java/com/satveerbrar/crm/Client.java
@@ -0,0 +1,135 @@
+package com.satveerbrar.crm;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Client {
+    private final SimpleStringProperty firstName;
+    private final SimpleStringProperty lastName;
+    private final SimpleStringProperty email;
+    private final SimpleStringProperty phoneNumber;
+    private final SimpleStringProperty reference;
+    private final SimpleStringProperty citizenship;
+    private final SimpleStringProperty applicationType;
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty notes;
+
+    public Client(String firstName, String lastName, String email, String phoneNumber, String reference, String citizenship, String applicationType, String date, String notes) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.email = new SimpleStringProperty(email);
+        this.phoneNumber = new SimpleStringProperty(phoneNumber);
+        this.reference = new SimpleStringProperty(reference);
+        this.citizenship = new SimpleStringProperty(citizenship);
+        this.applicationType = new SimpleStringProperty(applicationType);
+        this.date = new SimpleStringProperty(date);
+        this.notes = new SimpleStringProperty(notes);
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber.get();
+    }
+
+    public SimpleStringProperty phoneNumberProperty() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber.set(phoneNumber);
+    }
+
+    public String getReference() {
+        return reference.get();
+    }
+
+    public SimpleStringProperty referenceProperty() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference.set(reference);
+    }
+
+    public String getCitizenship() {
+        return citizenship.get();
+    }
+
+    public SimpleStringProperty citizenshipProperty() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+        this.citizenship.set(citizenship);
+    }
+
+    public String getApplicationType() {
+        return applicationType.get();
+    }
+
+    public SimpleStringProperty applicationTypeProperty() {
+        return applicationType;
+    }
+
+    public void setApplicationType(String applicationType) {
+        this.applicationType.set(applicationType);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public SimpleStringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
+    }
+
+    public String getNotes() {
+        return notes.get();
+    }
+
+    public SimpleStringProperty notesProperty() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes.set(notes);
+    }
+}
--- src/main/java/com/satveerbrar/crm/ViewCustomersController.java
+++ src/main/java/com/satveerbrar/crm/ViewCustomersController.java
@@ -1,4 +1,82 @@
 package com.satveerbrar.crm;
 
-public class ViewCustomersController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class ViewCustomersController implements Initializable {
+    @FXML
+    private TableView<Client> clientsTable;
+    @FXML
+    private TableColumn<Client, String> colFirstName;
+    @FXML
+    private TableColumn<Client, String> colLastName;
+    @FXML
+    private TableColumn<Client, String> colEmail;
+    @FXML
+    private TableColumn<Client, String> colPhoneNumber;
+    @FXML
+    private TableColumn<Client, String> colReference;
+    @FXML
+    private TableColumn<Client, String> colCitizenship;
+    @FXML
+    private TableColumn<Client, String> colApplicationType;
+    @FXML
+    private TableColumn<Client, String> colDate;
+    @FXML
+    private TableColumn<Client, String> colNotes;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        colFirstName.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
+        colLastName.setCellValueFactory(new PropertyValueFactory<>("LastName"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("Email"));
+        colPhoneNumber.setCellValueFactory(new PropertyValueFactory<>("PhoneNumber"));
+        colReference.setCellValueFactory(new PropertyValueFactory<>("Reference"));
+        colCitizenship.setCellValueFactory(new PropertyValueFactory<>("Citizenship"));
+        colApplicationType.setCellValueFactory(new PropertyValueFactory<>("ApplicationType"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        colNotes.setCellValueFactory(new PropertyValueFactory<>("Notes"));
+
+        loadClientData();
+    }
+
+    private void loadClientData() {
+        ObservableList<Client> clients = FXCollections.observableArrayList();
+        DatabaseConnection dbConnection = new DatabaseConnection();
+        Connection conn = dbConnection.getConnection();
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM clients")) {
+
+            while (rs.next()) {
+                clients.add(new Client(
+                        rs.getString("FIRST_NAME"),
+                        rs.getString("LAST_NAME"),
+                        rs.getString("EMAIL"),
+                        rs.getString("PHONE_NUMBER"),
+                        rs.getString("REFERENCE"),
+                        rs.getString("CITIZENSHIP"),
+                        rs.getString("APPLICATION_TYPE"),
+                        rs.getString("DATE"),
+                        rs.getString("NOTES")
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println("Error fetching clients: " + e.getMessage());
+        }
+        clientsTable.setItems(clients);
+    }
 }
