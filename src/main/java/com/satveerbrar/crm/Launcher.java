e076b08d9c5b755cf339280147cf0c6380f96e0c
Satveer Brar <satveerbrar180@gmail.com>
2024-04-30 20:33:54 -0400

    Database Initializer added and Login controller fixed to prevent sql injection

--- src/main/java/com/satveerbrar/crm/Launcher.java
+++ src/main/java/com/satveerbrar/crm/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        DatabaseInitializer.initializeDatabase();
         Application.launch(main.class, args);
     }
 }
