f6f17363a6bc49c84e3fe131b0b971084377b2ee
Satveer Brar <satveerbrar180@gmail.com>
2024-05-01 22:13:50 -0400

    add utility mehtod to apply hover cursor to multiple labels

--- src/main/java/com/satveerbrar/crm/ButtonUtils.java
+++ src/main/java/com/satveerbrar/crm/ButtonUtils.java
@@ -1,26 +1,11 @@
 package com.satveerbrar.crm;
 import javafx.scene.Cursor;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
 
 
 public class ButtonUtils {
-    public static void setHoverCursor(Button button) {
-        button.setOnMouseEntered(e -> {
-            button.setCursor(Cursor.HAND);
-        });
-
-        button.setOnMouseExited(e -> {
-            button.setCursor(Cursor.DEFAULT);
-        });
-    }
-    public static void setHoverCursor(Label label) {
-        label.setOnMouseEntered(e -> {
-            label.setCursor(Cursor.HAND);
-        });
-
-        label.setOnMouseExited(e -> {
-            label.setCursor(Cursor.DEFAULT);
-        });
+    public static void setHoverCursor(Node node) {
+        node.setOnMouseEntered(e -> node.setCursor(Cursor.HAND));
+        node.setOnMouseExited(e -> node.setCursor(Cursor.DEFAULT));
     }
 }
